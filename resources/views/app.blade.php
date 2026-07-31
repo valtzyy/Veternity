c727eddbb06dc941ac5ba6c515a020d2160487ef
Si-ID <noval27nov@gmail.com>
2026-07-31 22:35:21 +0700

    feat: initialize application layout, add favicon, and refine dockerignore rules

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
